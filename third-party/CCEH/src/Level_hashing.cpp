1623ba91bbdeac69551019a66f8d95b7d25ec533
yuhao-su <1464563510@qq.com>
2020-02-17 00:58:40 +0800

    backup

--- third-party/CCEH/src/Level_hashing.cpp
+++ third-party/CCEH/src/Level_hashing.cpp
@@ -30,13 +30,15 @@ LevelHashing::LevelHashing(void){
 
 LevelHashing::~LevelHashing(void){
   delete [] mutex;
-  delete [] buckets;
+  delete [] buckets[0];
+  delete [] buckets[1];
+
 }
 
 LevelHashing::LevelHashing(size_t _levels)
   : levels{_levels},
-  addr_capacity{pow(2, levels)},
-  total_capacity{pow(2, levels) + pow(2, levels-1)},
+  addr_capacity{(uint64_t)pow(2, levels)},
+  total_capacity{(uint64_t)pow(2, levels) + pow(2, levels-1)},
   resize_num{0}
 {
   locksize = 256;
